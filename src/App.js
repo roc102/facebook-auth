48cb4dd3c7f4d3bcfd688eaaab1b6adada332c55
roc102 <mdfahadrahman1969@gmail.com>
2024-02-04 19:24:49 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { FacebookLoginButton } from "react-social-login-buttons";
+import { LoginSocialFacebook } from "reactjs-social-login";
+
+const handleFacebookLogin = async (response) => {
+  try {
+    console.log("Response: ", response);
+  } catch (error) {
+    console.log("error: ", error);
+  }
+};
+
+const FacebookAuthComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoginSocialFacebook
+      appId="Your App Id"
+      onResolve={handleFacebookLogin}
+      onReject={(error) => {
+        console.log("Facebook login failed:", error);
+      }}
+    >
+      <FacebookLoginButton />
+    </LoginSocialFacebook>
   );
-}
+};
 
-export default App;
+export default FacebookAuthComponent;
